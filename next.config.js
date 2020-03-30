ab8d0dd06eaa791aca8980449731b90bd19c825c
Son Nguyen Van <son.nguyenvan@pycogroup.com>
2020-03-30 22:56:56 +0700

    fix: API_ENDPOINT

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-const withCss = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-
-module.exports = withCss(withSass())
+module.exports = {
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT
+  }
+}
